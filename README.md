96836eb9442aaad4e5954f1c89aaee05a46c61f7
Preston Zen <prestonzen@kaizenapps.com>
2023-12-23 23:31:20 +0400

    Alpha Release

---
+++ README.md
@@ -0,0 +1,44 @@
+# Sharky Snacker
+
+Sharky Snacker is an engaging arcade-style game where players help Sharky, the friendly shark, dodge the cunning Slavic Witch and munch on delicious pelmeni. Proudly designed by the imaginative team at Kaizen Gaming, our indie studio.
+
+## Gameplay
+
+![Sharky Snacker Gameplay](demos/Alpha1GameDemo.mp4)
+
+Navigate through the treacherous waters, outswim the Slavic Witch, and collect as many pelmeni as you can. Each level increases in difficulty, challenging your reflexes and strategy.
+
+### Controls
+
+- Use the arrow keys or `W`, `A`, `S`, `D` to move Sharky around.
+- Dodge the Slavic Witch while collecting pelmeni and hearts.
+- Press `Space` to play again upon game over.
+
+### Features
+
+- Responsive controls for fluid navigation.
+- Progressive difficulty with each level for engaging gameplay.
+- Retro-inspired graphics paired with charming sound effects.
+- An energetic soundtrack that keeps you in the zone.
+
+![Kaizen Gaming Logo](assets/KaizenGaming.png)
+
+## Installation and Prerequisites
+
+To play Sharky Snacker, you'll need Python and Pygame installed on your system.
+
+```sh
+pip install pygame
+```
+
+After installing the prerequisites, follow these steps:
+
+1. Clone this repository or download the ZIP file.
+2. Navigate to the game's directory.
+3. Run the game with python sharky_snacker.py.
+
+## Credits
+Sharky Snacker is crafted with ❤️ by Kaizen Gaming. A huge shoutout to our dedicated team members for bringing this game to life.
+
+### License
+Sharky Snacker is shared under the DBAD (Don't Be a Dick) License. Feel free to use and share this game, but always give credit where it's due.
--- game.py
+++ game.py
@@ -10,7 +10,7 @@ pygame.mixer.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 CELL_SIZE = 20
 GRID_WIDTH, GRID_HEIGHT = SCREEN_WIDTH // CELL_SIZE, SCREEN_HEIGHT // CELL_SIZE
-PELMENI_SEQUENCE = [1, 2, 3, 5, 8, 13]
+PELMENI_SEQUENCE = [1, 2, 3, 5, 8, 13, 21, 34]
 ENEMY_SPEED = CELL_SIZE // 2
 
 # Set up the display
@@ -24,12 +24,14 @@ pelmeni_image = pygame.image.load('assets/pelmeniSingle-70x70p.png').convert_alp
 sharky_image_original = pygame.image.load('assets/Sharky-485x280p.png').convert_alpha()
 enemy_image_original = pygame.image.load('assets/MilaSnape.png').convert_alpha()
 heart_image = pygame.image.load('assets/heart.png').convert_alpha()
+kaizen_logo = pygame.image.load('assets/KaizenGaming2-750x750p.png').convert_alpha()
 
 # Scale images to fit the grid size
 sharky_image = pygame.transform.scale(sharky_image_original, (CELL_SIZE, CELL_SIZE))
-enemy_image = pygame.transform.scale(enemy_image_original, (CELL_SIZE * 4, CELL_SIZE * 4))  # Enemy is 4x larger
+enemy_image = pygame.transform.scale(enemy_image_original, (CELL_SIZE * 4, CELL_SIZE * 3))  # Enemy is 3x larger
 pelmeni_image = pygame.transform.scale(pelmeni_image, (CELL_SIZE, CELL_SIZE))
 heart_image = pygame.transform.scale(heart_image, (CELL_SIZE, CELL_SIZE))
+kaizen_logo = pygame.transform.scale(kaizen_logo, (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
 
 # Sharky images for each direction
 sharky_image_left = sharky_image  # Original image is facing left
@@ -52,6 +54,13 @@ score = 0
 level = 0
 pelmeni_count = PELMENI_SEQUENCE[level]
 enemy_direction = [choice([-1, 1]), choice([-1, 1])]
+game_over = False
+
+# Show splash screen
+screen.fill((0, 0, 0))
+screen.blit(kaizen_logo, (SCREEN_WIDTH // 4, SCREEN_HEIGHT // 4))
+pygame.display.update()
+pygame.time.wait(3000)  # Show splash screen for 3 seconds
 
 # Function to place pelmeni and hearts
 def place_items():
@@ -81,115 +90,134 @@ enemy_x, enemy_y = randint(0, SCREEN_WIDTH - CELL_SIZE * 4), randint(0, SCREEN_H
 # Main game loop
 running = True
 while running:
-    # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # Shark movement and direction logic
-    keys = pygame.key.get_pressed()
-    move_x, move_y = 0, 0
-    if keys[pygame.K_LEFT]:
-        move_x = -1
-    if keys[pygame.K_RIGHT]:
-        move_x = 1
-    if keys[pygame.K_UP]:
-        move_y = -1
-    if keys[pygame.K_DOWN]:
-        move_y = 1
-
-    # Update Sharky's current image based on diagonal movement
-    if move_x == -1 and move_y == -1:  # Moving up and left
-        sharky_current_image = pygame.transform.rotate(sharky_image_left, -45)
-    elif move_x == 1 and move_y == -1:  # Moving up and right
-        sharky_current_image = pygame.transform.rotate(sharky_image_right, 45)
-    elif move_x == -1 and move_y == 1:  # Moving down and left
-        sharky_current_image = pygame.transform.rotate(sharky_image_left, 45)
-    elif move_x == 1 and move_y == 1:  # Moving down and right
-        sharky_current_image = pygame.transform.rotate(sharky_image_right, -45)
-    elif move_x == -1:
-        sharky_current_image = sharky_image_left
-    elif move_x == 1:
-        sharky_current_image = sharky_image_right
-    elif move_y == -1:
-        sharky_current_image = sharky_image_up
-    elif move_y == 1:
-        sharky_current_image = sharky_image_down
-
-    # Ensure Sharky stays within bounds
-    shark_x = max(0, min(GRID_WIDTH - 1, shark_x + move_x))
-    shark_y = max(0, min(GRID_HEIGHT - 1, shark_y + move_y))
-
-    # Enemy movement logic
-    enemy_x += enemy_direction[0] * ENEMY_SPEED
-    enemy_y += enemy_direction[1] * ENEMY_SPEED
-
-    # Make the enemy bounce off the walls
-    if enemy_x <= 0 or enemy_x + CELL_SIZE * 4 >= SCREEN_WIDTH:
-        enemy_direction[0] *= -1
-    if enemy_y <= 0 or enemy_y + CELL_SIZE * 4 >= SCREEN_HEIGHT:
-        enemy_direction[1] *= -1
-
-    # Drawing the grid and items
-    screen.fill((0, 0, 0))
-    for y in range(GRID_HEIGHT):
-        for x in range(GRID_WIDTH):
-            item = grid[y][x]
-            if item == 'pelmeni':
-                screen.blit(pelmeni_image, (x * CELL_SIZE, y * CELL_SIZE))
-            elif item == 'heart':
-                screen.blit(heart_image, (x * CELL_SIZE, y * CELL_SIZE))
-
-    # Check for collisions with pelmeni or heart
-    current_cell = grid[shark_y][shark_x]
-    if current_cell == 'pelmeni':
-        grid[shark_y][shark_x] = ''  # Clear the pelmeni from the grid
-        eating_sound.play()
-        score += 10  # Increment score
-        pelmeni_count -= 1
-    elif current_cell == 'heart':
-        grid[shark_y][shark_x] = ''  # Clear the heart from the grid
-        lives += 1  # Increment lives
-
-    # Check if level is complete
-    if pelmeni_count <= 0:
-        level += 1
-        if level < len(PELMENI_SEQUENCE):
+            running = False
+        if game_over and event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            # Reset game state for a new game
+            game_over = False
+            score = 0
+            level = 0
+            lives = 3
             pelmeni_count = PELMENI_SEQUENCE[level]
-            grid = place_items()  # Place new items for the next level
-        else:
-            print("You've completed the game!")  # Implement game completion logic
-            running = False
+            grid = place_items()
+            shark_x, shark_y = GRID_WIDTH // 2, GRID_HEIGHT // 2
+            enemy_x, enemy_y = randint(0, SCREEN_WIDTH - CELL_SIZE * 4), randint(0, SCREEN_HEIGHT - CELL_SIZE * 4)
+            pygame.mixer.music.play(-1)  # Restart music
 
-    # Draw the shark
-    screen.blit(sharky_current_image, (shark_x * CELL_SIZE, shark_y * CELL_SIZE))
+    if not game_over:
+        # Shark movement and direction logic
+        keys = pygame.key.get_pressed()
+        move_x, move_y = 0, 0
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            move_x = -1
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            move_x = 1
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            move_y = -1
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            move_y = 1
 
-    # Draw the enemy
-    screen.blit(enemy_image, (enemy_x, enemy_y))
+        # Update Sharky's position and current image based on movement
+        shark_x = max(0, min(GRID_WIDTH - 1, shark_x + move_x))
+        shark_y = max(0, min(GRID_HEIGHT - 1, shark_y + move_y))
+        if move_x == -1:
+            sharky_current_image = sharky_image_left
+        elif move_x == 1:
+            sharky_current_image = sharky_image_right
+        if move_y == -1:
+            sharky_current_image = sharky_image_up
+        elif move_y == 1:
+            sharky_current_image = sharky_image_down
 
-    # Check for collision with enemy
-    if (enemy_x <= shark_x * CELL_SIZE <= enemy_x + CELL_SIZE * 4 and
+        # Enemy movement logic with wall bouncing
+        enemy_x += enemy_direction[0] * ENEMY_SPEED
+        enemy_y += enemy_direction[1] * ENEMY_SPEED
+        if enemy_x <= 0 or enemy_x + CELL_SIZE * 4 >= SCREEN_WIDTH:
+            enemy_direction[0] *= -1
+        if enemy_y <= 0 or enemy_y + CELL_SIZE * 4 >= SCREEN_HEIGHT:
+            enemy_direction[1] *= -1
+
+        # Drawing the grid and items
+        screen.fill((0, 0, 0))
+        for y in range(GRID_HEIGHT):
+            for x in range(GRID_WIDTH):
+                if grid[y][x] == 'pelmeni':
+                    screen.blit(pelmeni_image, (x * CELL_SIZE, y * CELL_SIZE))
+                elif grid[y][x] == 'heart':
+                    screen.blit(heart_image, (x * CELL_SIZE, y * CELL_SIZE))
+
+        # Collision detection and scoring logic
+        current_cell = grid[shark_y][shark_x]
+        if current_cell == 'pelmeni':
+            grid[shark_y][shark_x] = ''  # Clear the pelmeni from the grid
+            eating_sound.play()
+            score += 10  # Increment score
+            pelmeni_count -= 1
+        elif current_cell == 'heart':
+            grid[shark_y][shark_x] = ''  # Clear the heart from the grid
+            lives += 1  # Increment lives
+
+        # Check if level is complete
+        if pelmeni_count <= 0:
+            level += 1
+            if level < len(PELMENI_SEQUENCE):
+                pelmeni_count = PELMENI_SEQUENCE[level]
+                grid = place_items()  # Place new items for the next level
+            else:
+                game_over = True  # No more levels, end the game
+
+        # Drawing Sharky, enemy, score, and lives
+        screen.blit(sharky_current_image, (shark_x * CELL_SIZE, shark_y * CELL_SIZE))
+        screen.blit(enemy_image, (enemy_x, enemy_y))
+
+        # Draw the score
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
+
+        # Draw the lives
+        for i in range(lives):
+            screen.blit(heart_image, (SCREEN_WIDTH - (i + 1) * (CELL_SIZE + 5), 10))
+
+        # Check for collision with enemy
+        if (enemy_x <= shark_x * CELL_SIZE <= enemy_x + CELL_SIZE * 4 and
             enemy_y <= shark_y * CELL_SIZE <= enemy_y + CELL_SIZE * 4):
-        death_sound.play()
-        lives -= 1
-        if lives <= 0:
-            print("Game Over")  # Implement game over logic
-            running = False
-        else:
+            death_sound.play()
+            lives -= 1
             shark_x, shark_y = GRID_WIDTH // 2, GRID_HEIGHT // 2  # Reset Sharky position
+            if lives <= 0:
+                game_over = True
 
-    # Draw the score
-    score_text = font.render(f'Score: {score}', True, (255, 255, 255))
-    screen.blit(score_text, (10, 10))
+        # Update display and tick
+        pygame.display.update()
+        clock.tick(15)  # Frame rate
 
-    # Draw the lives
-    for i in range(lives):
-        screen.blit(heart_image, (SCREEN_WIDTH - (i + 1) * (CELL_SIZE + 5), 10))
+    else:
+        # Game over screen
+        screen.fill((0, 0, 0))
+        game_over_text = font.render('Game Over', True, (255, 0, 0))
+        screen.blit(game_over_text, (SCREEN_WIDTH // 2 - game_over_text.get_width() // 2, SCREEN_HEIGHT // 2 - game_over_text.get_height() // 2))
+        play_again_text = font.render('Press Space to Play Again', True, (255, 255, 255))
+        screen.blit(play_again_text, (SCREEN_WIDTH // 2 - play_again_text.get_width() // 2, SCREEN_HEIGHT // 2 + game_over_text.get_height()))
+        pygame.display.update()
 
-    # Update display
-    pygame.display.update()
-    clock.tick(15)  # Frame rate
+        # Wait for space key to reset the game after game over
+        while game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    game_over = False
+                    score = 0
+                    level = 0
+                    lives = 3
+                    pelmeni_count = PELMENI_SEQUENCE[0]
+                    grid = place_items()
+                    shark_x, shark_y = GRID_WIDTH // 2, GRID_HEIGHT // 2
+                    enemy_x, enemy_y = randint(0, SCREEN_WIDTH - CELL_SIZE * 4), randint(0, SCREEN_HEIGHT - CELL_SIZE * 4)
+                    pygame.mixer.music.play(-1)  # Restart music
+                    break  # Break out of the inner loop to restart the game loop
 
 # Clean up
 pygame.quit()
